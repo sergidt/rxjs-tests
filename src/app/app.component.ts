975294a4740ab194fdd2c4862ff0e741825985f8
sergi.dote <anna1106>
2017-07-12 17:34:44 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,21 @@
 import { Component } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
+
+
+const vehicleTypes = ['Car', 'Bike', 'Truck'];
+
+const getRangeValue = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+class Vehicle {
+  constructor(public type: string, public speed: number){}
+
+  toString(){
+    return `${this.type} at ${this.speed} km/h`;
+  }
+}
+
+const generateVehicle = () => new Vehicle(vehicleTypes[getRangeValue(0, vehicleTypes.length - 1)], getRangeValue(60, 120));
 
 @Component({
   selector: 'app-root',
@@ -7,4 +24,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor() {
+    const subject$ = new Subject<any>();
+
+    setInterval(() => {
+      subject$.next(generateVehicle());
+    }, 100);
+
+/*    Observable.timeInterval(100)
+              .map(_ => generateVehicle())
+              // .windowTime(1000)
+              .average(x => x.speed)
+              .subscribe(subject$);*/
+
+
+
+    subject$.subscribe(console.log);
+  }
 }
