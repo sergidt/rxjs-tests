2cb628f17ab7f21db7544b80987bae81f6a37c6c
Sergi <sergidt@gmail.com>
2017-07-13 07:35:35 +0200

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/observable/interval';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/bufferTime';
+import 'rxjs/add/operator/do';
 import { Observable } from 'rxjs/Observable';
 
 
@@ -8,11 +13,9 @@ const vehicleTypes = ['Car', 'Bike', 'Truck'];
 const getRangeValue = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
 class Vehicle {
-  constructor(public type: string, public speed: number){}
+  constructor(public type: string, public speed: number) {}
 
-  toString(){
-    return `${this.type} at ${this.speed} km/h`;
-  }
+  toString = () => `${this.type} at ${this.speed} km/h`;
 }
 
 const generateVehicle = () => new Vehicle(vehicleTypes[getRangeValue(0, vehicleTypes.length - 1)], getRangeValue(60, 120));
@@ -28,9 +31,12 @@ export class AppComponent {
   constructor() {
     const subject$ = new Subject<any>();
 
-    setInterval(() => {
-      subject$.next(generateVehicle());
-    }, 100);
+      Observable.interval(100)
+                .map(_ => generateVehicle())
+                .bufferTime(1000)
+                .switchMap(x => x)
+                .do(_ => console.log('WINDOW---------------------------------'))
+                .subscribe(subject$);
 
 /*    Observable.timeInterval(100)
               .map(_ => generateVehicle())
@@ -40,6 +46,7 @@ export class AppComponent {
 
 
 
-    subject$.subscribe(console.log);
+    subject$
+    .subscribe(console.log);
   }
 }
