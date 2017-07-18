4202c759f3057b6080827172fa50ef9cc96a3955
Sergi <sergidt@gmail.com>
2017-07-18 23:58:00 +0200

    changes

--- src/app/components/earth-quakes/earth-quakes-frp.ts
+++ src/app/components/earth-quakes/earth-quakes-frp.ts
@@ -5,13 +5,12 @@ interface Earthquake {
     title: string;
     place: string;
     mag: number;
-    type: string;
 }
 
 const EARTH_QUAKES_URL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
 const TYPE = 'earthquake';
 const SITUATION = 'California';
-const MAGNITUDE = 1.5;
+const MAGNITUDE = 3;
 
 function httpGetAsync(URL): Observable<any> {
     return Observable.create(observer => {
@@ -26,37 +25,12 @@ function httpGetAsync(URL): Observable<any> {
     });
 }
 
-const extractData = (data) => Observable.of(JSON.parse(data).features.map((x: any) => x.properties));
-
-function filterEarthquakes(data) {
-    let items = [];
-    for (const item of data) {
-        const properties = item.properties;
-        if (properties.type === TYPE && ~properties.place.indexOf(SITUATION) && properties.mag > MAGNITUDE) {
-            items.push(item);
-        }
-    }
-    return items;
-}
-
-function groupByMagnitude(earthquakes) {
-    let groups = {};
-
-    for (let eq of earthquakes) {
-        const earthquake: Earthquake = eq.properties;
-        const mag = Math.floor(earthquake.mag).toString();
-        if (!groups[mag]){
-            groups[mag] = [earthquake.title];
-        }else {
-            groups[mag] = [...groups[mag], earthquake.title];
-        }
-    }
-    return groups;
-}
+const extractData = (data) : Observable<any> => Observable.from(JSON.parse(data).features.map((x: any) => x.properties));
 
 export function mainFRP() {
     httpGetAsync(EARTH_QUAKES_URL)
     .switchMap(extractData)
-//    .switchMap((earthquakes: Array<Earthquake>)=> );
+    .filter((earthquake: Earthquake) => earthquake.place.indexOf(SITUATION) > -1)
+    .filter((earthquake: Earthquake) => earthquake.mag > MAGNITUDE)
     .subscribe(console.log);
 }
--- src/app/components/earth-quakes/earth-quakes-promises.ts
+++ src/app/components/earth-quakes/earth-quakes-promises.ts
@@ -5,12 +5,11 @@ interface Earthquake {
     type: string;
 }
 
-let filteredData;
+let earthquakes;
 
 const EARTH_QUAKES_URL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
-const TYPE = 'earthquake';
 const SITUATION = 'California';
-const MAGNITUDE = 1.5;
+const MAGNITUDE = 3;
 
 function httpGetAsync(URL): Promise<any> {
     return new Promise((resolve, _) => {
@@ -30,40 +29,24 @@ function extractData(data) {
 }
 
 function applyFilters(data) {
-    const earthquakes = filterEarthquakes(data);
-    filteredData = groupByMagnitude(earthquakes);
+    earthquakes = filterEarthquakes(data);
 }
 
 function filterEarthquakes(data) {
     let items = [];
     for (const item of data) {
         const properties = item.properties;
-        if (properties.type === TYPE && ~properties.place.indexOf(SITUATION) && properties.mag > MAGNITUDE) {
+        if (~properties.place.indexOf(SITUATION) && properties.mag > MAGNITUDE) {
             items.push(item);
         }
     }
     return items;
 }
 
-function groupByMagnitude(earthquakes) {
-    let groups = {};
-
-    for (let eq of earthquakes) {
-        const earthquake: Earthquake = eq.properties;
-        const mag = Math.floor(earthquake.mag).toString();
-        if (!groups[mag]){
-            groups[mag] = [earthquake.title];
-        }else {
-            groups[mag] = [...groups[mag], earthquake.title];
-        }
-    }
-    return groups;
-}
-
 export function mainPromises() {
     httpGetAsync(EARTH_QUAKES_URL)
         .then(data => {
             extractData(data);
-            console.log(filteredData);
+            console.log(earthquakes);
         });
 }
